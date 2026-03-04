f8420f983d92c41af1bab8ad9e009832e6dfdcf5
HappyGhastQwQ <gihgiu14@gmail.com>
2026-03-04 10:25:41 +0700

    Update postStartCommand.sh

--- .devcontainer/postStartCommand.sh
+++ .devcontainer/postStartCommand.sh
@@ -9,11 +9,11 @@ sysctl --write net.ipv6.conf.all.forwarding=1
 ethtool --features eth0 rx-udp-gro-forwarding on
 ethtool --features eth0 rx-gro-list off
 
-swapoff /tmp/swap
-fallocate --length 16G /tmp/swap
-chmod 600 /tmp/swap
-mkswap /tmp/swap
-swapon /tmp/swap
+# swapoff /tmp/swap
+# fallocate --length 16G /tmp/swap
+# chmod 600 /tmp/swap
+# mkswap /tmp/swap
+# swapon /tmp/swap
 
 dockerd --seccomp-profile unconfined --experimental &> /dev/null &
 
